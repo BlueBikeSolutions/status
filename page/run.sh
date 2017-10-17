67d10e2d9a0588cd487b9e2ba027b509883aab96
Ricky Cook <mail@thatpanda.com>
2017-10-17 15:20:20 +1100

    Remove Jenkins debugging

--- page/run.sh
+++ page/run.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 set -e
-set -x
 yarn run build
 
 for environment in prod nonprod; do
-  echo "== $environment =="
-  ls /code
-  ls /code/services
-  cat /code/services/$environment.json
   aws s3 sync /code/build/ s3://$environment.status.bluebike.hosting/
   aws s3 cp /code/services/$environment.json s3://$environment.status.bluebike.hosting/services.json
 done
