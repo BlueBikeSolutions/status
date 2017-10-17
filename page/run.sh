373939f42c86e6c6da0ffb9f419dc697d10a059e
Ricky Cook <mail@thatpanda.com>
2017-10-17 12:05:34 +1100

    Fix react build path

--- page/run.sh
+++ page/run.sh
@@ -3,6 +3,6 @@ set -e
 yarn run build
 
 for environment in prod nonprod; do
-  aws s3 sync /code/dist/ s3://bbs-status-$environment/
+  aws s3 sync /code/build/ s3://bbs-status-$environment/
   aws s3 cp /code/services/$environment.json s3://bbs-status-$environment/services.json
 done
