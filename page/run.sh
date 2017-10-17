8e00d3742c7b8387b1aa713c76c1b8e9246514cd
Ricky Cook <mail@thatpanda.com>
2017-10-17 15:14:30 +1100

    Add some Jenkins debugging

--- page/run.sh
+++ page/run.sh
@@ -3,6 +3,10 @@ set -e
 yarn run build
 
 for environment in prod nonprod; do
+  echo "== $environment =="
+  ls /code
+  ls /code/services
+  cat /code/services/$environment.json
   aws s3 sync /code/build/ s3://$environment.status.bluebike.hosting/
   aws s3 cp /code/services/$environment.json s3://$environment.status.bluebike.hosting/services.json
 done
